16f04f52bb334bbb91fcfacfed33de74ea8875a5
KritiGandotra <kritigandotra@gmail.com>
2020-08-02 00:24:38 +0530

    Updates

---
+++ precache-manifest.ef0d9542b5954a286436b34d4bcf20f1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e78bff9433796389ea6e45f37dd0bba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aaa9fa80d20268c08e20",
+    "url": "/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "cd3bf54fb940117128b2",
+    "url": "/static/css/main.32f1795b.chunk.css"
+  },
+  {
+    "revision": "aaa9fa80d20268c08e20",
+    "url": "/static/js/2.00460214.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/static/js/2.00460214.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd3bf54fb940117128b2",
+    "url": "/static/js/main.f0aacb64.chunk.js"
+  },
+  {
+    "revision": "f0683ef68012e3bf0299",
+    "url": "/static/js/runtime-main.e38d7b65.js"
+  },
+  {
+    "revision": "df4ce4090390e942f1349f4d1e088b12",
+    "url": "/static/media/MythsInfographic-(002)_800.df4ce409.jpg"
+  },
+  {
+    "revision": "9cbe43fbc1b923865c9aa5d16b1af410",
+    "url": "/static/media/blog-august-1200x480.9cbe43fb.jpg"
+  },
+  {
+    "revision": "14fda528f4d8fd26a950f713977f4f7c",
+    "url": "/static/media/download.14fda528.png"
+  },
+  {
+    "revision": "91b854f80eb4d4b3d87b61f9ffaf8a43",
+    "url": "/static/media/ehealth-tech.91b854f8.png"
+  },
+  {
+    "revision": "3ac3f97a59e507631dcd9188302714c6",
+    "url": "/static/media/yf_logo.3ac3f97a.jpeg"
+  }
+]);
